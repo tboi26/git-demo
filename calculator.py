f7a1724e8aaeb31f6a3c0857509deb692927b85b
Garen Malek <garen.malek@gmail.com>
2022-10-17 16:00:50 +0200

    Updated readme.

--- calculator.py
+++ calculator.py
@@ -2,8 +2,11 @@
 def calculate():
     operation = input("Adjon meg egy műveleti jelet: ")
 
-    if operation != '+' or operation != '-' or operation != '*' or operation != '/':
-        return calculate()
+    if operation == '+' or operation == '-' or operation == '*' or operation == '/':
+        break
+        
+    else:
+        return calculate
 
     number_1 = int(input('Első szám: '))
     number_2 = int(input('Második szám: '))
